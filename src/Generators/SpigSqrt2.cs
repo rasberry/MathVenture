011665faecb9e37ba320c74a2817e2eed31bb0fc
rasberry <rasberry@hotmail.com>
2020-01-15 22:55:00 -0700

    removed non-spigotcore versions

--- src/Generators/SpigSqrt2.cs
+++ src/Generators/SpigSqrt2.cs
@@ -24,20 +24,3 @@ namespace SequenceGen.Generators
 		}
 	}
 }
-
-/*
-#!/usr/local/bin/ruby
-k, a, b, a1, b1 = 2, 3, 2, 7, 5
-loop do
-  p, q, k = 1, 2, k+1
-  a, b, a1, b1 = a1, b1, p*a+q*a1, p*b+q*b1
-  d = a / b
-  d1 = a1 / b1
-  while d == d1
-    print d
-    $stdout.flush
-    a, a1 = 10*(a%b), 10*(a1%b1)
-    d, d1 = a/b, a1/b1
-  end
-end
-*/
