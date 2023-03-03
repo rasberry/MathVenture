c9bf07929378bba7fce2970fbc7188bd2e8b0ae8
rasberry <rasberry@hotmail.com>
2023-03-02 20:35:28 -0700

    POC for random LSFR seems to be working

--- src/LFSR/Start.cs
+++ src/LFSR/Start.cs
@@ -19,6 +19,33 @@ public class Start : IMain
 		//for(int i=2; i<=16; i++) {
 		//	TestLFSR.TryAll(i);
 		//}
-		PrimitivePoly.FindAll(25);
+
+		PrimitivePolyOne.FindAll(9);
+		Log.Debug(new string('=',80));
+
+		/*
+		var n2b = new necklace2bitpol(9);
+
+		for(ulong a = 0; a < 512; a++) {
+			bool w = PrimitivePoly.TryPrim(a,9);
+			Log.Debug($"a = {a} w = {w}");
+			if (w) {
+				var sb = new StringBuilder();
+				ulong p = n2b.poly(a);
+				Bits.bitpol_print_coeff(sb,"",p);
+				//Log.Debug($"p = {p} coeff = {sb}");
+				Log.Debug(sb.ToString());
+			}
+		}
+		*/
+
+		for(int i=0; i < 10; i++) {
+			var t = PrimitivePoly.GetRandomTaps(9);
+
+			var sb = new StringBuilder();
+			Bits.bitpol_print_coeff(sb,"",t);
+
+			Log.Debug($"taps = {t} coeff = {sb}");
+		}
 	}
 }
